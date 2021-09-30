67dd78d537f21fe3d48ba279b00a069c446c31c5
George Sebik <waffilka@gmail.com>
2021-09-30 14:21:08 +0300

    added commands

---
+++ Notification.Application/Commands/Broker/SubscriptionCommand.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using Notification.Application.Handler;
+using Notification.Application.Logger;
+using Notification.Contracts.DataTransferObject.Broker;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Notification.Application.Commands.Broker
+{
+    public class SubscriptionCommand : IRequest
+    {
+        public SubscriptionCommand(Subscription subscription)
+        {
+            Subscription = subscription;
+        }
+
+        public Subscription Subscription { get; set; }
+    }
+
+    public class SubscriptionCommandHandler : LoggerRequestHandler<SubscriptionCommand, Unit>
+    {
+        public SubscriptionCommandHandler(ILoggerManager logger)
+            : base(logger)
+        { }
+
+        public async override Task<Unit> HandleInternalAsync(SubscriptionCommand request, CancellationToken cancellationToken)
+        {
+            //...
+            return Unit.Value;
+        }
+    }
+}
---
+++ Notification.Application/Commands/Broker/UnsubscriptionCommand.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using Notification.Application.Handler;
+using Notification.Application.Logger;
+using Notification.Contracts.DataTransferObject.Broker;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Notification.Application.Commands.Broker
+{
+    public class UnsubscriptionCommand : IRequest
+    {
+        public UnsubscriptionCommand(Unsubscription unsubscription)
+        {
+            Unsubscription = unsubscription;
+        }
+
+        public Unsubscription Unsubscription { get; set; }
+    }
+
+    public class UnsubscriptionCommandHandler : LoggerRequestHandler<UnsubscriptionCommand, Unit>
+    {
+        public UnsubscriptionCommandHandler(ILoggerManager logger)
+            : base(logger)
+        { }
+
+        public async override Task<Unit> HandleInternalAsync(UnsubscriptionCommand request, CancellationToken cancellationToken)
+        {
+            //...
+            return Unit.Value;
+        }
+    }
+
+}
