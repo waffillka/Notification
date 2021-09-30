8a8a213479a27636587accc21cd44f4e13bc22d7
George Sebik <waffilka@gmail.com>
2021-09-30 14:19:29 +0300

    entity

--- Notification.Data/Entities/Book.cs
+++ Notification.Data/Entities/Book.cs
@@ -1,12 +1,14 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Notification.Data.Entities
 {
     public class Book : EntityBase
     {
+        public Guid BookId { get; set; }
         public string Name { get; set; }
         public string ISBN { get; set; }
-        public string Authors { get; set; }
+        public ICollection<string> Authors { get; set; }
 
         public ICollection<User> Users { get; set; }
     }
