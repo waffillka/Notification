b2747e10d47d7b50ce9713e5fa74261142592308
George Sebik <waffilka@gmail.com>
2021-10-04 11:10:54 +0300

    change broker model

--- Notification.Contracts/DataTransferObject/Broker/Notification.cs
+++ Notification.Contracts/DataTransferObject/Broker/Notification.cs
@@ -6,5 +6,6 @@ namespace Notification.Contracts.DataTransferObject.Broker
     public class Notification : INotification
     {
         public Guid BookId { get; set; }
+        public Guid UserId { get; set; }
     }
 }
